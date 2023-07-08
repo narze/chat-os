3b4f9a56ade303822406c0475b3a4a309e8ff06c
Manassarn Manoonchai <manassarn@gmail.com>
2023-07-09 00:52:23 +0700

    Notify when timer is ended if user allows browser notification

--- src/lib/commands/components/Timer.svelte
+++ src/lib/commands/components/Timer.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { onDestroy } from 'svelte';
+	import AlertIcon from './AlertIcon.svelte';
+	import AlertDisabledIcon from './AlertDisabledIcon.svelte';
 
 	export let options: Record<string, string> = {};
 
@@ -8,8 +10,10 @@
 	const endAt = startAt + timerSeconds * 1000;
 	const name = options.name || undefined;
 
+	let ended = endAt - Date.now() <= 0;
 	let msLeft: number;
 	let frame: number;
+	let notificationState: NotificationPermission = Notification.permission;
 
 	(function update() {
 		frame = requestAnimationFrame(update);
@@ -19,6 +23,16 @@
 		if (msLeft <= 0) {
 			msLeft = 0;
 			cancelAnimationFrame(frame);
+
+			if (!ended) {
+				ended = true;
+
+				if (notificationState == 'granted') {
+					new Notification('Timer ended', {
+						body: name ? `Timer ${name} ended` : 'Timer ended'
+					});
+				}
+			}
 		}
 	})();
 
@@ -41,4 +55,29 @@
 	<div class="text-7xl break-all">
 		{format(Math.ceil(msLeft / 1000))}
 	</div>
+
+	{#if msLeft > 0}
+		{#if notificationState == 'default'}
+			<button
+				class="btn btn-outline btn-circle btn-sm mt-2"
+				on:click={() =>
+					Notification.requestPermission().then((state) => (notificationState = state))}
+			>
+				<AlertIcon />
+			</button>
+		{:else if notificationState == 'granted'}
+			<button class="btn btn-circle btn-success btn-sm mt-2">
+				<AlertIcon />
+			</button>
+		{:else if notificationState == 'denied'}
+			<button
+				class="btn btn-outline btn-circle btn-danger btn-sm mt-2"
+				on:click={() => {
+					alert('Notifications are blocked. Please unblock on your browser.');
+				}}
+			>
+				<AlertDisabledIcon />
+			</button>
+		{/if}
+	{/if}
 </div>
