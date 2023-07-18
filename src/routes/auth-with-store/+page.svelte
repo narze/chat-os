2b21284ce042820d4274aa3b9070fb8ede36e42b
Manassarn Manoonchai <manassarn@gmail.com>
2023-07-18 22:52:26 +0700

    Add another page with minimal code

---
+++ src/routes/auth-with-store/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { userStore } from "$lib/firebase-store";
+
+  const user = userStore()
+</script>
+
+{#if $user === undefined}
+  <p>Loading...</p>
+{:else if $user}
+  <p>Logged in as {$user.displayName}</p>
+{:else}
+  <p>
+    Not logged in
+  </p>
+{/if}
