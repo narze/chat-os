3aa0ca21b5d83a3958d280d8f45b80fad4b0e2b9
Manassarn Manoonchai <manassarn@gmail.com>
2023-07-18 22:39:26 +0700

    Add document

--- src/lib/firebase-store.ts
+++ src/lib/firebase-store.ts
@@ -2,6 +2,11 @@ import { writable } from 'svelte/store';
 import { auth } from './firebase';
 import type { User } from 'firebase/auth';
 
+/**
+ * The userStore is updated whenever the authentication state changes.
+ * If the authentication is not initialized or not in the browser, the store returns undefined.
+ * Therefore, you could check for undefined in the component and show a loading or blank state.
+ */
 export function userStore() {
 	// Handle SSR by returning $user as undefined, then check in the component as loading state
 	if (!auth || !globalThis.window) {
